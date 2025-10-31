82351a4dd04e2ff8fc71fe2e52157d3797cb65ca
heaven443 <hcortez44359@liceo.edu.ph>
2025-10-31 18:54:25 +0800

    Fix : Unable to run

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.clarity.app {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires eu.hansolo.tilesfx;
+    requires javafx.graphics;
+
+    opens com.clarity.app to javafx.fxml;
+    exports com.clarity.app;
+}
